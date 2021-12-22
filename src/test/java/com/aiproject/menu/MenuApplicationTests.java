3bf44f43f3865b298a5dc1b134e6f7bdd8181211
aimarkhwang <ai.markhwang@gmail.com>
2021-12-22 15:17:00 +0900

    menu페이지를 springboot 버전으로

---
+++ src/test/java/com/aiproject/menu/MenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiproject.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
